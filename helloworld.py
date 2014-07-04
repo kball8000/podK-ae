381fe9326cce23cb3b4dbfa50f2bc7e07436bdaa
Kevin Ball <kball8000@gmail.com>
2014-07-04 14:41:59 -0400

    added links between 2 pages

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/4597121/podcatchor/styles/podK.css">')
         self.response.write('</head>')
         self.response.write('<h1>Header should be blue</h1>')
-        self.response.write('Two!<br>')
+        self.response.write('<a href="http://kball-test-tools.appspot.com/second">Second page</a><br>')
         self.response.write('</body></html>')
 
 class SecondPage(webapp2.RequestHandler):
@@ -16,6 +16,7 @@ class SecondPage(webapp2.RequestHandler):
         self.response.write('Hello, World!')
         self.response.write('Page 2!<br>')
         self.response.write('<p>any errors</p>')
+        self.response.write('<a href="http://kball-test-tools.appspot.com/">Main page</a><br>')
 
 app = webapp2.WSGIApplication([
     (r'/', MainPage),
