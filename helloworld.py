383841ea7a199ca3d0efef05f55ad40df1126564
Kevin Ball <kball8000@gmail.com>
2014-07-04 13:45:39 -0400

    trying html

--- helloworld.py
+++ helloworld.py
@@ -2,18 +2,18 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
         self.response.write('Hello, World!<br>')
         self.response.write('Two!')
 
 class SecondPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
         self.response.write('Hello, World!')
         self.response.write('Page 2!<br>')
-        self.response.write('any errors')
+        self.response.write('<p>any errors</p>')
 
-application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/second', SecondPage),
+app = webapp2.WSGIApplication([
+    (r'/', MainPage),
+    (r'/second', SecondPage),
 ], debug=True)
