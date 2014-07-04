4825bdf59cb6f24432a685f69fc8d090d66bdada
Kevin Ball <kball8000@gmail.com>
2014-07-04 15:55:56 -0400

    turned off appengine import to see if that will make the page load

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-from google.appengine.api import users
+# from google.appengine.api import users
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
@@ -8,17 +8,17 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/4597121/podcatchor/styles/podK.css">')
         self.response.write('</head>')
         self.response.write('<h1>Header should be blue</h1>')
-        self.response.write('<a href="http://kball-test-tools.appspot.com/second">Second page</a><br>')
+        self.response.write('<a href="http://kball-test-tools.appspot.com/second">Second page</a>')
         self.response.write('</body></html>')
 
 class SecondPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
+        # user = users.get_current_user()
         self.response.headers['Content-Type'] = 'text/html'
-        if user:
-            self.response.write('Hello, %s (<a href="%s">Sign out</a>)' % (user.get_nickname(), users.create_logout_url('/'))
-        else:
-            self.response.write('<a href="%s">Click here to login</a>' %s users.create_login_url(self.request.uri)
+        # if user:
+            # self.response.write('Hello, %s (<a href="%s">Sign out</a>)' % (user.get_nickname(), users.create_logout_url('/'))
+        # else:
+            # self.response.write('<a href="%s">Click here to login</a>' %s users.create_login_url(self.request.uri)
         self.response.write('Page 2!<br>')
         self.response.write('<p>any errors</p>')
         self.response.write('<a href="http://kball-test-tools.appspot.com/">Main page</a><br>')
