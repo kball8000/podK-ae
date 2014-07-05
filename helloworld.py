e0f58ff9d8e526b73ff8387e8e08513337612311
Kevin Ball <kball8000@gmail.com>
2014-07-05 16:14:50 -0400

    changed to D

--- helloworld.py
+++ helloworld.py
@@ -54,7 +54,7 @@ class MainPage(webapp2.RequestHandler):
         logging.info('Hello, looging is working...')        
 
 # For revving so I know when I"ve got a new page
-        self.response.write('<h1>HeaderB</h1>')
+        self.response.write('<h1>HeaderD</h1>')
 
 
         self.response.write('<h2><a href="http://kball-test-tools.appspot.com/second">Second page</a></h2>')
