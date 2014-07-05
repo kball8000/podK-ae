53c51c00733134c5a855c58493e52a760d156606
Kevin Ball <kball8000@gmail.com>
2014-07-05 14:52:56 -0400

    debugging post of podcast feed

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ from google.appengine.ext import ndb
 import cgi
 import webapp2
 import urllib
+import logging
 
 FORM_HTML = """\
 podcast feed
@@ -15,8 +16,8 @@ podcast feed
 # Greeting          = PodcastFeed
 # guestbook_name    = podcast_feed_list = default_podcast_feed_list
 # 'Guestbook'       = 'podcast_feed_list' for nbd
-# greetings         = podcastfeeds
-# greeting          = podcastfeed
+# greetings         = podcast_feeds
+# greeting          = podcast_feed
 
 DEFAULT_PODCAST_FEED_LIST = 'default_podcast_feed_list'
 # http://feeds.twit.tv/twit.xml
@@ -39,12 +40,18 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('</head>')
 
         podcast_feed_list = self.request.get('podcast_feed', DEFAULT_PODCAST_FEED_LIST)
+
+# Debug code
+        self.response.write('podcast_feed_list = %s <br>' % podcast_feed_list)
+        self.response.write('podcast_feed_key = %s <br>' % podcast_feed_key(podcast_feed_list))
+
         podcast_feed_query = PodcastFeed.query(
             ancestor = podcast_feed_key(podcast_feed_list)).order(-PodcastFeed.date)
         podcast_feeds = podcast_feed_query.fetch(10)
-        
         for feed in podcast_feeds:
             self.response.write('%s added %s<br>' %(feed.content, feed.date))
+
+        logging.info('Hello, looging is working...')        
 
         self.response.write('<h1>Header</h1>')
         self.response.write('<h2><a href="http://kball-test-tools.appspot.com/second">Second page</a></h2>')
@@ -56,7 +63,12 @@ class Podcasts(webapp2.RequestHandler):
         podcast_feed_list = self.request.get('podcast_feed_list', DEFAULT_PODCAST_FEED_LIST)
         podcast_feed = PodcastFeed(parent=podcast_feed_key(podcast_feed_list))
         podcast_feed.content = self.request.get('content')
-        podcast_feed.put()
+
+# Debubbing code
+        self.response.write('podcast feed content = %s <br>' % podcast_feed.content)
+        self.response.write('podcast feed list = %s <br>' % podcast_feed_list)
+        
+        # podcast_feed.put()
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
@@ -69,10 +81,12 @@ class SecondPage(webapp2.RequestHandler):
         user = users.get_current_user()
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write('<html><body><head>')
-        self.response.write('<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/4597121/podcatchor/styles/podK.css">')
+        self.response.write('<link type="text/css" rel="stylesheet" href="/stylesheets/helloworld.css">')
+        self.response.write('<link type="text/css" rel="stylesheet" href="/stylesheets/helloworld.css">')
+        # self.response.write('<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/4597121/podcatchor/styles/podK.css">')
         self.response.write('</head>')
         if user:
-            self.response.write('<h1>Hello, %s, you are logged in!A</h1>' % user.nickname())
+            self.response.write('<h1>Hello, %s, you are logged in!B</h1>' % user.nickname())
             # self.response.write('Hello, %s (<a href="%s">Sign out</a>)' % (user.nickname(), users.create_logout_url('/'))
         else:
             self.redirect(users.create_login_url(self.request.uri))
