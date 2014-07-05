a01f2fac14f713580c3a7ee587781e9b8f61864a
Kevin Ball <kball8000@gmail.com>
2014-07-04 21:45:16 -0400

    added a submit form

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,13 @@
 from google.appengine.api import users
-
+import cgi
 import webapp2
+
+FORM_HTML = """
+<form action="/sign" method="post">
+    <div><textarea name='content' rows="3" columns="60"></textarea></div>
+    <div><input type="submit" value = "Sign Guestbook"></div>
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -8,9 +15,15 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('<html><body><head>')
         self.response.write('<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/4597121/podcatchor/styles/podK.css">')
         self.response.write('</head>')
-        self.response.write('<h1>Header should be blue</h1>')
-        self.response.write('<a href="http://kball-test-tools.appspot.com/second">Second page</a>')
+        self.response.write('<h1><a href="http://kball-test-tools.appspot.com/second">Second page</a></h1>')
+        self.response.write(FORM_HTML)
         self.response.write('</body></html>')
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>You wrote<pre>')
+        self.response.write(cgi.escape(self.response.get('content')))
+        self.response.write('</pre></body></html>')
 
 class SecondPage(webapp2.RequestHandler):
     def get(self):
@@ -20,20 +33,16 @@ class SecondPage(webapp2.RequestHandler):
         self.response.write('<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/4597121/podcatchor/styles/podK.css">')
         self.response.write('</head>')
         if user:
-            self.response.write('<h1>Hello, you ARE logged in</h1><br>')
-            self.response.write('<h1>Hello, %s</h1>' % user.nickname())
-            # self.response.write('Hello, %s (<a href="%s">Sign out</a>)' % (user.get_nickname(), users.create_logout_url('/'))
+            self.response.write('<h1>Hello, %s, you are logged in!</h1>' % user.nickname())
+            # self.response.write('Hello, %s (<a href="%s">Sign out</a>)' % (user.nickname(), users.create_logout_url('/'))
         else:
             self.redirect(users.create_login_url(self.request.uri))
-            # self.response.write('<h1>Herro, you are not logged in</h1><br>')
-            # self.response.write('User is not logged in to google')
-            # self.response.write('<a href="%s">Click here to login</a>' % users.create_login_url(self.request.uri)
-        self.response.write('Page 2!<br>')
-        self.response.write('<p>any errors</p>')
+            # self.response.write('You are not logged in <a href="%s">Click here to login</a>' % users.create_login_url(self.request.uri)
         self.response.write('<a href="http://kball-test-tools.appspot.com/">Main page</a><br>')
         self.response.write('</body></html>')
 
 app = webapp2.WSGIApplication([
-    (r'/', MainPage),
-    (r'/second', SecondPage),
+    ('/', MainPage),
+    ('/sign', Guestbook),
+    ('/second', SecondPage),
 ], debug=True)
