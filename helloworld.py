7f8719ae6b7e5ab1b42ed3a949b845f94c2507f6
Kevin Ball <kball8000@gmail.com>
2014-07-07 18:55:21 -0400

    added more user info to welcome portion

--- helloworld.py
+++ helloworld.py
@@ -73,7 +73,9 @@ class MainPage(webapp2.RequestHandler):
         if not user:
             self.response.write('<a href="%s">Sign In<br></a>' % users.create_login_url(self.request.uri))
         else:
-            self.response.write('Welcome %s!' % user.nickname())
+            self.response.write('Welcome %s!<br>' % user.nickname())
+            self.response.write('Welcome %s!<br>' % user.user_id())
+            self.response.write('Welcome %s!<br>' % user.email())
 
         podcast_feed_list = self.request.get('podcast_feed', DEFAULT_PODCAST_FEED_LIST)
 
@@ -99,7 +101,7 @@ class MainPage(webapp2.RequestHandler):
         # self.response.write('<script>console.log("Logging is working: %s")</script>' % podcast_feed_list)
 
 # For revving so I know when I"ve got a new page
-        self.response.write('<h1>HeaderB</h1>')
+        self.response.write('<h1>HeaderD</h1>')
         self.response.write('<h2><a href="http://kball-test-tools.appspot.com/second">Second page</a></h2>')
         self.response.write('http://feeds.twit.tv/twit.xml<br>')
         self.response.write('http://feeds.twit.tv/sn.xml<br>')
