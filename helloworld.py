fa464c2b07b397497eae571a03b471c742c713f6
Kevin Ball <kball8000@gmail.com>
2014-07-12 15:50:29 -0400

    removing data again 3

--- helloworld.py
+++ helloworld.py
@@ -93,7 +93,7 @@ class MainPage(webapp2.RequestHandler):
         # self.response.write('<script>console.log("Logging is working: %s")</script>' % podcast_feed_list)
 
 # For revving so I know when I"ve got a new page
-        self.response.write('<h1>HeaderA</h1>')
+        self.response.write('<h1>HeaderB</h1>')
         self.response.write('<h2><a href="http://kball-test-tools.appspot.com/second">Second page</a></h2>')
         self.response.write('http://feeds.twit.tv/twit.xml<br>')
         self.response.write('http://feeds.twit.tv/sn.xml<br>')
@@ -141,7 +141,7 @@ class remPodcastFeed(webapp2.RequestHandler):
     
         # podcast_feed = ndb.Key(PodcastFeed, int(feed_id), parent=ndb.Key('podcast_feed', 'default_podcast_feed_list'))
         podcast_feed = ndb.Key(PodcastFeed, 1, parent=ndb.Key('podcast_feed', 'default_podcast_feed_list'))
-        podcast_feed.key.delete()
+        podcast_feed.delete()
         
         # query_params = {'podcast_feed_list' : podcast_feed_list}
         # self.redirect('/?' + urllib.urlencode(query_params))
