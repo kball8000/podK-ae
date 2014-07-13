fe82f4383b7e3e44222c5fa2a26337acf2da9994
Kevin Ball <kball8000@gmail.com>
2014-07-13 14:31:51 -0400

    playing my current favorite show

--- helloworld.py
+++ helloworld.py
@@ -3,14 +3,33 @@ from google.appengine.ext import ndb
 import webapp2
 import urllib
 import urllib2
-from xml.etree import ElementTree
+import xml.etree.ElementTree as ET
 # from xmldom import minidom
 # import html
 # import cgi
 
 # TO DO:
+# Ask what is going on with fancy redirects.
 # Would like to make less queries, so all podcast feeds should be in a list
 # then all episode information for each show could be in 1 entity
+# Ask huber if how I"m handling controls is proper. Seems odd to have urls as 'controls', i.e. to remove feeds.
+# Put an are you sure / undo / store removed shows somewhere / maybe even a remove forever button once it's on the removed list.
+
+# **-- Need to store:  --**
+# 
+# podcast rss feeds in array (i'm not clear why i'm storing them separately, will probably move to show object
+#   (maybe there will be a speed advantage, but at the moment, I can't think of one.)
+# 
+# object of each show including (on average probably 5 - 10 of these) - This is what we will store in datastore
+#   rss feed
+#   show name
+#   show hosts???
+#   episode object (1 - 100)
+#     episode name
+#     episode url
+#     listened boolean
+#     last listened location
+#     episode length???
 
 FORM_HTML = """\
 <form action="/addpodcast" method="post">
@@ -93,42 +112,38 @@ class MainPage(webapp2.RequestHandler):
         podcast_feeds = podcast_feed_query.fetch(10)
 
         self.response.write('<br><br>**Current saved feeds from datastore:<br>')
-        shows = [1, 2, 3]
+        shows = xrange(3)
 
         playerName = 'myMusic1'
-        defaultEp = 'http://www.podtrac.com/pts/redirect.mp3/twit.cachefly.net/audio/sn/sn0461/sn0461.mp3'
+        defaultEp = 'http://www.podtrac.com/pts/redirect.mp3/twit.cachefly.net/audio/sn/sn0461/sn0462.mp3'
         selectedEp = 'http://www.podtrac.com/pts/redirect.mp3/twit.cachefly.net/audio/twig/twig0257/twig0257.mp3'
 
         for feed in podcast_feeds:
             self.response.write('<form action="/rempodcast" method="post"> %s <input type="hidden" name="delRecord" value="%s"><input type="submit" \
             value="x"></form>' % (feed.content, feed.key.id()))
             self.response.write('<form action="/getfeed" method="post"><input type="hidden" name="getFeed" value="%s"><input type="submit" \
-            value="Refresh"></form>' % feed.content)
+            value="&#8635 OR &#10227"></form>' % feed.content)
             self.response.write('<div class="podcastFeedList"><ul>')
             for show in shows:
-                self.response.write("""<li>Episode %s <a onclick="myAudio.playSelectedEpisode('%s')" class="playButton">Play</a> \
+                self.response.write("""<li>Episode %s <a onclick="myAudio.playSelectedEpisode('%s')" class="playButton">&#9658</a> \
                 </li>""" % (show, selectedEp))
             self.response.write('</ul></div>')
             
-            # % (feed.content, feed.key.id(), podcast_feeds.index(feed)))
-            # self.response.write('<br>')
-
         self.response.write(MUSIC_CONTROLS_HTML % (playerName, defaultEp))
 
 
         # How to write to the javascript console log in the browser
         # self.response.write('<script>console.log("Logging is working: %s")</script>' % podcast_feed_list)
 
-# For revving so I know when I"ve got a new page
-        self.response.write('<h1>HeaderD</h1>')
-        self.response.write('<h2><a href="http://kball-test-tools.appspot.com/second">Second page</a></h2>')
-        self.response.write('http://feeds.twit.tv/twit.xml<br>')
-        self.response.write('http://feeds.twit.tv/sn.xml<br>')
+# ****-----  For revving so I know when I"ve got a new page  ----****
+        self.response.write('<h1>HeaderE</h1>')
+        self.response.write('http://feeds.twit.tv/sn.xml ep 456 at 12 min<br>')
+        self.response.write('Swap out the "sn" with "twig" / "twit" / "mbw" or any other twit show to try out other feeds<br>')
         self.response.write(FORM_HTML)
         self.response.write('<script src="/scripts/podK.js"></script>')
         self.response.write('</body></html>')
 
-class Podcasts(webapp2.RequestHandler):
+class AddPodcasts(webapp2.RequestHandler):
     def post(self):
         podcast_feed_list = self.request.get('podcast_feed_list', DEFAULT_PODCAST_FEED_LIST)
 
@@ -143,11 +158,9 @@ class Podcasts(webapp2.RequestHandler):
 
         podcast_feed.put()
 
-        query_params = {'podcast_feed_list' : podcast_feed_list}
-        self.redirect('/?' + urllib.urlencode(query_params))
-        # self.redirect('/')
+        self.redirect('/')
 
-class getFeed(webapp2.RequestHandler):
+class GetFeed(webapp2.RequestHandler):
     def post(self):
         
         self.response.headers['Content-Type'] = 'text/html'
@@ -155,27 +168,25 @@ class getFeed(webapp2.RequestHandler):
         self.response.write('<link type="text/css" rel="stylesheet" href="/stylesheets/helloworld.css">')
         self.response.write('</head>')
 
-        
         url = self.request.get('getFeed')
-        
+        request = urllib2.Request(url)
         try:
-            xmlFromWeb = urllib2.urlopen(url).read()
-
+            response = urllib2.urlopen(request).read()
         except urllib2.URLError, e:
             self.response.write('could not refresh feed')
             
-        xml = ElementTree.fromstring(xmlFromWeb)
+        xml = ET.fromstring(response)
         self.response.write(xml.findtext(".//title"))
             
-        # for f in url:
-        #     self.response.write(f)
-
         self.response.write('</body></html>')
 
-        # self.redirect('/')
+        self.redirect('/')
 
-class remPodcastFeed(webapp2.RequestHandler):
+class RemPodcastFeed(webapp2.RequestHandler):
     def post(self):
+        
+        # Get id from post request and delete that show from list. 
+        # Also double checks with user,  by way of javascript that they really want to do this.
     
         feed_id = self.request.get('delRecord')
         
@@ -219,10 +230,10 @@ class SecondPage(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     (r'/', MainPage),
-    (r'/addpodcast', Podcasts),
-    ('/rempodcast', remPodcastFeed),
+    (r'/addpodcast', AddPodcasts),
+    ('/rempodcast', RemPodcastFeed),
     # (r'/rempodcast/(\d+)', remPodcastFeed),
-    ('/getfeed', getFeed),
+    ('/getfeed', GetFeed),
     # ('/sign', Guestbook),
     ('/second', SecondPage),
 ], debug=True)
