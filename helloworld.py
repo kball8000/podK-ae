bdf29bc3243e845e154b27e2605ec92169da6457
Kevin Ball <kball8000@gmail.com>
2014-07-13 22:20:35 -0400

    working on refresh 5

--- helloworld.py
+++ helloworld.py
@@ -136,7 +136,7 @@ class MainPage(webapp2.RequestHandler):
         # self.response.write('<script>console.log("Logging is working: %s")</script>' % podcast_feed_list)
 
 # ****-----  For revving so I know when I"ve got a new page  ----****
-        self.response.write('<h1>HeaderB</h1>')
+        self.response.write('<h1>HeaderC</h1>')
         self.response.write('http://feeds.twit.tv/sn.xml ep 456 at 12 min<br>')
         self.response.write('Swap out the "sn" with "twig" / "twit" / "mbw" or any other twit show to try out other feeds<br>')
         self.response.write(FORM_HTML)
@@ -178,8 +178,7 @@ class GetFeed(webapp2.RequestHandler):
         root = ET.fromstring(response)
         for child in root:
             self.response.write('child tag: %s, child attrib: %s' %(child.tag, child.attrib))
-        self.response.write(xml.findtext(".//title"))
-            
+
         self.response.write('</body></html>')
 
         self.redirect('/')
