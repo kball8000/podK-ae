977ae92e140b354f3b8ec4f8c086aaf52373319a
Kevin Ball <kball8000@gmail.com>
2014-07-14 22:03:44 -0400

    me vs. JSON 3

--- helloworld.py
+++ helloworld.py
@@ -144,7 +144,7 @@ class MainPage(webapp2.RequestHandler):
         # self.response.write('<script>console.log("Logging is working: %s")</script>' % podcast_feed_list)
 
 # ****-----  For revving so I know when I"ve got a new page  ----****
-        self.response.write('<h1>HeaderB</h1>')
+        self.response.write('<h1>HeaderC</h1>')
         self.response.write('http://feeds.twit.tv/sn.xml ep 456 at 12 min<br>')
         self.response.write('Swap out the "sn" with "twig" / "twit" / "mbw" or any other twit show to try out other feeds<br>')
         self.response.write(FORM_HTML)
@@ -179,7 +179,8 @@ class SearchITunes(webapp2.RequestHandler):
         response = urllib2.urlopen(request)
         readResponse = response.read()
         # data = json.load(readResponse)
-        data = json.load(response)
+        jsonData = json.open(response)
+        data = json.load(jsonData)
         for a in data:
             self.response.write('<br>JSON line: %s' %a)
 
