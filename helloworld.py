ddc64c4f12b10821661702e38d742366207dfdea
Kevin Ball <kball8000@gmail.com>
2014-07-26 18:39:47 +0000

    updating page 2 - testing data store queries 22

--- helloworld.py
+++ helloworld.py
@@ -195,9 +195,13 @@ class AddPodcast(webapp2.RequestHandler):
             podcast.author = users.get_current_user()
             
         podcast.feedUrl = self.request.get('formContent')
-        podcast.show = [Episode(title='year', listened=False),]
+        for x in xrange(3):
+            podcast.show = [Episode(title='year %s', listened=False),] % x
+            podcast.put()
 
-        podcast.put()
+        # podcast.show = [Episode(title='year', listened=False),]
+        # podcast.put()
+
 
         self.redirect('/')
 
