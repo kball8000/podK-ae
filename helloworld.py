4f6086259cc542af25a939c3609697b7a2159334
Kevin Ball <kball8000@gmail.com>
2014-07-26 13:22:19 +0000

    updating page 2 - testing data store queries 15 typo

--- helloworld.py
+++ helloworld.py
@@ -296,7 +296,7 @@ class SecondPage(webapp2.RequestHandler):
 
         for feed in podcast_feeds:
             self.response.write('feed url: %s and feed id: %s <br>' % (feed.feedUrl, feed.key.id()))
-            self.response.write('show title: %s <br>' % (feed.show.title))
+            # self.response.write('show title: %s <br>' % (feed.show.title))
 
         # if user:
         #     self.response.write('<h1>Hello, %s, you are logged in!B</h1>' % user.nickname())
