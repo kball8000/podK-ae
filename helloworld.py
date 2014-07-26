5b894a65dcbe11e092495c3b520dc2930abf586b
Kevin Ball <kball8000@gmail.com>
2014-07-26 11:57:51 +0000

    updating page 2 - testing data store queries 9 removed all

--- helloworld.py
+++ helloworld.py
@@ -284,7 +284,8 @@ class SecondPage(webapp2.RequestHandler):
  
         podcast_feed_query = Podcast.query(ancestor = podcast_feed_key(podcast_feed_list)).order(-Podcast.date)
         # podcast_feed_query = Podcast.query()
-        podcast_feeds = podcast_feed_query.fetch(all)
+        podcast_feeds = podcast_feed_query.fetch()
+        self.response.write('blah')
 
         self.response.write('<br><br>**Current saved feeds from datastore ** C ** :<br>')
         # shows = xrange(3)
