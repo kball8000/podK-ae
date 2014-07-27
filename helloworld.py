21a6114068020c8162cdfdd7670f599c910f69b8
Kevin Ball <kball8000@gmail.com>
2014-07-27 02:19:09 +0000

    updating page 2 - testing data store queries 42

--- helloworld.py
+++ helloworld.py
@@ -112,7 +112,7 @@ class MainPage(webapp2.RequestHandler):
 
         # Heading 1
         self.response.write('<h1>PodKatchor</h1>')
-        self.response.write('<h2>Pretty much the best online podcast player **** <b>B</b> **** </h2>')
+        self.response.write('<h2>Pretty much the best online podcast player **** <b>C</b> **** </h2>')
 
         #Have user log in and show their current subscriptions.
         if user:
@@ -207,7 +207,7 @@ class AddPodcast(webapp2.RequestHandler):
 
         # self.redirect('/')
 
-        return self.redirect('/')
+        return self.redirect('/', True)
 
 class SearchITunes(webapp2.RequestHandler):
     def post(self):
