9619fa61a16aff3b0f8dead1bea4cddeae253838
Kevin Ball <kball8000@gmail.com>
2014-07-27 13:23:54 +0000

    delete a structured property entry 14

--- helloworld.py
+++ helloworld.py
@@ -148,7 +148,7 @@ class MainPage(webapp2.RequestHandler):
 
                 if feed.show.index(show) == 3:
                     self.response.write('<br>about to remove feed... %s' % feed.show[1])
-                    feed.show[3].key.delete()
+                    feed.show[3].delete()
 
 
             # for show in shows:
