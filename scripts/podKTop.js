b3e653bfca984c35e264283f358615bd7760180e
kball8000 <kball8000@gmail.com>
2014-08-03 18:25:05 -0500

    Improved iTunes Search Results display
    sort by title and grouped by same title without anything in parenthesis.

--- scripts/podKTop.js
+++ scripts/podKTop.js
@@ -1,10 +1,8 @@
 $( function(){
 	//Search iTunes when user hits enter key on input field.
 	$( "#iTunesSearchValue" ).keypress(function(e){
-		console.log('in enter key mode.');
 		var key = e.which;
 		if(key === 13){
-			console.log('in enter key mode.2');
 			sendITunesSearchRequest();
 			return false;
 		}
@@ -33,19 +31,93 @@ $( function(){
 	});
 });
 
+function getBasePodcast(longName){
+	var shortName = '';
+	var nameLength = longName.length;
+	var i = 0;
+	while(i < nameLength){
+		if(longName[i] !== '('){
+			shortName += longName[i];
+			i += 1;
+		}
+		else{
+			break;
+		}
+	}
+	return shortName;
+}
+
+function createPodcastListItem (result){
+	var html = '<li>' +
+		result.collectionCensoredName + 
+		'<a href="' + result.feedUrl + '">Subscribe<\/a>' + 
+		'<\/li>';
+
+	return html;
+}
+
+
+
 function showITunesSearchResults(arg){
 	//This callback function of the dynamically loaded script display the restults from iTunes store.
 	var results = arg.results;
-	var html = '<ul>';
-	html = '';
-	for(var i=0; i<arg.resultCount; i++){
-		html += '<li>' +
-			'<img src="' + results[i].artworkUrl60 + '"\/>' +		
-			results[i].collectionCensoredName + 
-			'<a href="' + results[i].feedUrl + '">Subscribe<\/a>' + 
-			'<\/li>';
+	var html = '';
+	var basePodcast = '';
+	var nextBasePodcast = '';
+	var isGridOpen = false;
+	var isNextPodcastSameTitle = false;
+	
+	function compareResultsObjects(a,b){
+		if (a.collectionName > b.collectionName){
+			return 1;
+		}
+		if (a.collectionName < b.collectionName){
+			return -1;
+		}
+		return 0;
 	}
-	html += '<\/ul>';
+	results.sort(compareResultsObjects);
+
+	
+// 	Get string of collection name without the '(' for comparing, figure out how to handle if last one in list
+// 	is the same as previous and therefore not closed out.
+	
+	// Display the results with image on the LHS and info on 80% RHS, attempted to remove repeating images
+	html = '<div class="podcastSearchResults">';
+	
+	if (arg.resultCount === 0) {
+		html = 'No results found';
+		$('#iTunesSearchResultsHtml').html(html);	
+		return true;
+	}
+	
+	for(var i=0; i<arg.resultCount; i++){
+		if(!isGridOpen){
+			html += '<div class="ui-grid-a">';
+			isGridOpen = true;
+			html += '<div class="ui-block-a">';
+			html += '<img src="' + results[i].artworkUrl60 + '"\/>';
+			html += '<\/div>'; // close block a
+			html += '<div class="ui-block-b">';
+			html += '<ul>';
+		}
+		html += createPodcastListItem(results[i]);
+		basePodcast = getBasePodcast(results[i].collectionCensoredName);
+		if (i < arg.resultCount - 1){
+			nextBasePodcast = getBasePodcast(results[ i+1 ].collectionCensoredName);
+			if (basePodcast === nextBasePodcast){
+				continue;
+			}	
+		}
+
+		if(isGridOpen){
+			html += '<\/ul>'; // close list of shows
+			html += '<\/div>'; // close block b
+			html += '<\/div>'; // close grid a
+			isGridOpen = false;
+		}
+	}
+	html += '<\/div>'; // Closes podcast search results 
 	$('#iTunesSearchResultsHtml').html(html);
 }
 
